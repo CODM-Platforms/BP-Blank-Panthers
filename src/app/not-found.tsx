9617f0bd85d2fff75e260bacff64be976e3b3fc4
Luqman Hamza <dcs22002@suza.ac.tz>
2026-09-25 11:30:40 +0300

    Changes

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,6 +6,7 @@ export default function NotFound() {
       <a href="/" className="mt-8 rounded-lg bg-blue-600 px-6 py-3 text-white hover:bg-blue-500">
         Return Home
       </a>
+      
     </main>
   );
 }
